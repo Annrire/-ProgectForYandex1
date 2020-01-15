a829c835c79b54c902f269c30c8f76bede33527e
Annrire <lap62002@mail.ru>
2020-01-15 06:10:52 +0300

    final

--- progect123.py
+++ progect123.py
@@ -1,5 +1,6 @@
 import pygame
 import csv
+import random
 from moviepy.editor import VideoFileClip 
 pygame.init()
 
@@ -96,6 +97,50 @@ def score8():
     text1 = font.render(str(scores[7][1]), 1, (0, 0, 0))     
     sc.blit(text1, [740, y11]) 
 
+def drawn(x1, y1, coolor):
+    global col_yellow
+    global col_green
+    global col_white
+    global col_orange
+    xx = (x1 - 504) // 41
+    yy = (y1 - 229) // 41
+    xxx = 501 + 41 * xx
+    yyy = 225 + 41 * yy
+    rects.append([coolor, (xxx, yyy, 39, 39)])
+    if coolor == (255, 255, 0):
+        if xx == 6 and (yy == 3 or yy == 4):
+            col_yellow += 1
+    if coolor == (255, 255, 255):
+        if xx == 1 or xx == 0 or xx == 7 or xx == 6:
+            if yy == 1 or yy == 0 or yy == 7 or yy == 6:
+                col_white += 1
+    if coolor == (0, 128, 0):
+        if xx == 3 or xx == 4 or xx == 5:
+            if yy == 0 or yy == 1 or yy == 2:
+                col_green += 1
+    if coolor == (255, 165, 0):
+        if yy == 0:
+            if xx == 2 or xx == 3 or xx == 5:
+                col_orange += 1
+        if yy == 1:
+            if xx == 1 or xx == 2 or xx == 6:
+                col_orange += 1
+        if yy == 2:
+            if xx != 4:
+                col_orange += 1
+        if yy == 3 or yy == 4:
+            if xx != 6:
+                col_orange += 1
+        if yy == 5:
+            col_orange += 1
+        if yy == 6:
+            if xx != 0 or xx != 7:
+                col_orange += 1
+        if yy == 7:
+            if xx == 2 or xx == 3 or xx == 4 or xx == 5:
+                col_orange += 1
+    
+
 
 pygame.display.set_caption('Äîáğî ïîæàëîâàòü â èãğó!') 
 sc = pygame.display.set_mode((1024, 768))
@@ -104,10 +149,16 @@ background_position = [0, 0]
 background_image = pygame.image.load("íà÷àëî.gif").convert()
 sc.blit(background_image, background_position)
 y1 = 0
+col_yellow = 0
+col_white = 0
+col_orange = 0
+col_green = 0
 x1 = 0
 shag = 0
 y11 = 0
 step = 0
+step1 = 0
+rects = []
 pygame.display.update()
 running = True
 start = False
@@ -118,11 +169,17 @@ dora1 = False
 vash_chet = False
 caracter = 1
 schet = 0
+num11, num12 = random.randint(0, 5), random.randint(0, 4)
+num21, num22 = random.randint(0, 5), random.randint(0, 4)
+num31, num32 = random.randint(0, 5), random.randint(0, 4)
+num41, num42 = random.randint(0, 5), random.randint(0, 4)
+num51, num52 = random.randint(0, 5), random.randint(0, 4)
 kolgames = []
 rightans = []
 name = False
 start_game = False 
 dora2 = False
+dora3 = False
 scores = []
 font1 = pygame.font.Font(None, 150)
 input_box = pygame.Rect(100, 300, 400, 100)
@@ -132,6 +189,9 @@ color = color_inactive
 active = False
 text1 = ''
 names = []
+numb_ans = ['']
+tappp = 0
+stroka = 0
 
 
 path = "records1.csv"
@@ -339,15 +399,19 @@ while game:
             font = pygame.font.Font(None, 130)
             text = font.render(str(schet), True, (0, 0, 0))
             sc.blit(text, [710, 134])        
-        if caracter % 3 == 1:
-            if x1 >= 177 and x1 <= 226 and y1 >= 481 and y1 <= 623:
-                if not 'dora1' in kolgames:
-                    start_game = False 
-                    dora1 = True
-            if x1 >= 442 and x1 <= 541 and y1 >= 490 and y1 <= 610:
-                if 'dora1' in kolgames and not 'dora2' in kolgames:
-                    start_game = False 
-                    dora2 = True
+        if x1 >= 177 and x1 <= 226 and y1 >= 481 and y1 <= 623:
+            if not 'dora1' in kolgames:
+                start_game = False 
+                dora1 = True
+        if x1 >= 442 and x1 <= 541 and y1 >= 490 and y1 <= 610:
+            if 'dora1' in kolgames and not 'dora2' in kolgames:
+                start_game = False 
+                dora2 = True
+                coolor = (255, 255, 255)
+        if x1 >= 720 and x1 <= 818 and y1 >= 489 and y1 <= 606:
+            if 'dora2' in kolgames and not 'dora3' in kolgames:
+                start_game = False 
+                dora3 = True
         if x1 >= 778 and x1 <= 918 and y1 >= 683 and y1 <= 756:
             # ñîxğàíèòü ğåçóëüòaò
             if len(kolgames) == 0:
@@ -507,6 +571,15 @@ while game:
                 if event.button == 1:
                     x1, y1 = event.pos
                     print(x1, 'qqq', y1)
+        if x1 >= 116 and x1 <= 219 and y1 >= 591 and y1 <= 694:
+            coolor = (255, 255, 255)
+        if x1 >= 256 and x1 <= 354 and y1 >= 593 and y1 <= 692:
+            coolor = (255, 165, 0)    
+        if x1 >= 396 and x1 <= 496 and y1 >= 593 and y1 <= 690:
+            coolor = (255, 255, 0)
+        if x1 >= 536 and x1 <= 633 and y1 >= 592 and y1 <= 692:
+            coolor = (0, 128, 0)
+        
         b1 = 8
         b2 = 8
         board = Board(b1, b2)
@@ -515,17 +588,239 @@ while game:
         a3 = 41
         board.set_view(a1, a2, a3)
         listt = []
-        running1 = True    
         background_position = [0, 0]
         background_image = pygame.image.load("dora2.gif").convert()
         sc.blit(background_image, background_position)
         board.render(sc)
-        
+        if x1 >= 504 and x1 <= 829 and y1 >= 229 and y1 <= 553:
+            drawn(x1, y1, coolor)       
+        for i in rects:
+            pygame.draw.rect(sc, i[0], i[1])
         if not 'dora2' in kolgames:
             kolgames.append('dora2')        
         if x1 >= 698 and x1 <= 915 and y1 >= 621 and y1 <= 667:
+            if col_yellow >= 2:
+                rightans.append('dora ye')
+                rightans.append('dora ye')
+                rightans.append('dora ye')
+            if col_white >= 12:
+                rightans.append('dora wi')
+                rightans.append('dora wi')                
+            if col_orange >= 45:
+                rightans.append('dora or')
+                rightans.append('dora or')
+                rightans.append('dora or')                
+            if col_green >= 5:
+                rightans.append('dora gr')
+                rightans.append('dora gr')                    
             dora2 = False
             vash_chet = True        
+        if start:
+            dr1 = image.get_rect(bottomright=(x + 44, y + 40))
+            sc.blit(image, dr1)
+        pygame.display.flip()
+        
+        
+    while dora3:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game = False
+                dora3 = False
+            if event.type == pygame.MOUSEMOTION:
+                x, y = event.pos        
+                start = True
+                image = pygame.image.load("arrow.png").convert_alpha()
+                sc.blit(background_image, background_position)
+                pygame.mouse.set_visible(False)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    x1, y1 = event.pos    
+                    if x1 >= 746 and x1 <= 943 and y1 >= 344 and y1 <= 378:
+                        step1 += 1
+                        
+                    if x1 >= 738 and x1 <= 944 and y1 >= 652 and y1 <= 693:
+                        step1 += 1     
+                        numb_ans = ['']
+                    
+                    print(x1, y1)
+        background_position = [0, 0]
+        background_image = pygame.image.load("dora3.gif").convert()
+        sc.blit(background_image, background_position)
+        font = pygame.font.Font(None, 90)
+        text = font.render('ãîòîâî', True, (0, 0, 0))
+        sc.blit(text, [700, 325])
+        if x1 >= 320 and x1 <= 361 and y1 >= 354 and y1 <= 426:
+            if '1' != numb_ans[0]:
+                numb_ans[0] = '1'
+        if x1 >= 429 and x1 <= 491 and y1 >= 352 and y1 <= 421:
+            if '2' != numb_ans[0]:
+                numb_ans[0] = '2'
+        if x1 >= 552 and x1 <= 618 and y1 >= 349 and y1 <= 423:
+            if '3' != numb_ans[0]:
+                numb_ans[0] = '3'
+        if x1 >= 298 and x1 <= 362 and y1 >= 455 and y1 <= 531:
+            if '4' != numb_ans[0]:
+                numb_ans[0] = '4'
+        if x1 >= 425 and x1 <= 489 and y1 >= 457 and y1 <= 530:
+            if '5' != numb_ans[0]:
+                numb_ans[0] = '5'
+        if x1 >= 544 and x1 <= 608 and y1 >= 463 and y1 <= 534:
+            if '6' != numb_ans[0]:
+                numb_ans[0] = '6'
+        if x1 >= 308 and x1 <= 352 and y1 >= 564 and y1 <= 640:
+            if '7' != numb_ans[0]:
+                numb_ans[0] = '7'
+        if x1 >= 422 and x1 <= 483 and y1 >= 564 and y1 <= 641:
+            if '8' != numb_ans[0]:
+                numb_ans[0] = '8'
+        if x1 >= 546 and x1 <= 600 and y1 >= 567 and y1 <= 642:
+            if '9' != numb_ans[0]:
+                numb_ans[0] = '9'
+        if x1 >= 654 and x1 <= 709 and y1 >= 565 and y1 <= 640:
+            if '0' != numb_ans[0]:
+                numb_ans[0] = '0'
+        
+        if step1 == 0:
+            font = pygame.font.Font(None, 120)
+            text = font.render(str(num11) + '  +  ' + str(num12) + '  =', True, (0, 0, 0))
+            sc.blit(text, [200, 203])
+            if numb_ans[0] != '':
+                font = pygame.font.Font(None, 120)
+                text = font.render(numb_ans[0], True, (0, 0, 0))
+                sc.blit(text, [600, 203])
+            shag = 1
+        if step1 == 2: 
+            font = pygame.font.Font(None, 120)
+            text = font.render(str(num21) + '  +  ' + str(num22) + '  =', True, (0, 0, 0))
+            sc.blit(text, [200, 203])
+            if numb_ans[0] != '':
+                font = pygame.font.Font(None, 120)
+                text = font.render(numb_ans[0], True, (0, 0, 0))
+                sc.blit(text, [600, 203])
+            shag = 2
+        if step1 == 4:
+            font = pygame.font.Font(None, 120)
+            text = font.render(str(num31) + '  +  ' + str(num32) + '  =', True, (0, 0, 0))
+            sc.blit(text, [200, 203])
+            if numb_ans[0] != '':
+                font = pygame.font.Font(None, 120)
+                text = font.render(numb_ans[0], True, (0, 0, 0))
+                sc.blit(text, [600, 203])
+            shag = 3
+        if step1 == 6:
+            font = pygame.font.Font(None, 120)
+            text = font.render(str(num41) + '  +  ' + str(num42) + '  =', True, (0, 0, 0))
+            sc.blit(text, [200, 203])
+            if numb_ans[0] != '':
+                font = pygame.font.Font(None, 120)
+                text = font.render(numb_ans[0], True, (0, 0, 0))
+                sc.blit(text, [600, 203])
+            shag = 4
+        if step1 == 8:
+            font = pygame.font.Font(None, 120)
+            text = font.render(str(num51) + '  +  ' + str(num52) + '  =', True, (0, 0, 0))
+            sc.blit(text, [200, 203])
+            if numb_ans[0] != '':
+                font = pygame.font.Font(None, 120)
+                text = font.render(numb_ans[0], True, (0, 0, 0))
+                sc.blit(text, [600, 203])
+            shag = 5
+            
+        if step1 % 2 == 1:
+            font = pygame.font.Font(None, 90)
+            text = font.render('äàëåå', True, (0, 0, 0))
+            sc.blit(text, [741, 640])
+            if shag == 1:
+                font = pygame.font.Font(None, 120)
+                text = font.render(str(num11) + '  +  ' + str(num12) + '  =', True, (0, 0, 0))
+                sc.blit(text, [200, 203])                
+                if int(numb_ans[0]) == num11 + num12:
+                    background_p = [805, 200]
+                    background_i = pygame.image.load("Check.gif").convert()
+                    sc.blit(background_i, background_p)     
+                    if not 'dora31' in rightans:
+                        rightans.append('dora31')
+                        rightans.append('dora31')     
+                else:
+                    background_p = [805, 200]
+                    background_i = pygame.image.load("none.gif").convert()
+                    sc.blit(background_i, background_p)  
+
+            if shag == 2:
+                font = pygame.font.Font(None, 120)
+                text = font.render(str(num21) + '  +  ' + str(num22) + '  =', True, (0, 0, 0))
+                sc.blit(text, [200, 203])                
+                if int(numb_ans[0]) == num21 + num22:
+                    background_p = [805, 200]
+                    background_i = pygame.image.load("Check.gif").convert()
+                    sc.blit(background_i, background_p)   
+                    if not 'dora32' in rightans:
+                        rightans.append('dora32')
+                        rightans.append('dora32')     
+
+                else:
+                    background_p = [805, 200]
+                    background_i = pygame.image.load("none.gif").convert()
+                    sc.blit(background_i, background_p)  
+                
+                
+            if shag == 3:
+                font = pygame.font.Font(None, 120)
+                text = font.render(str(num31) + '  +  ' + str(num32) + '  =', True, (0, 0, 0))
+                sc.blit(text, [200, 203])                
+                if int(numb_ans[0]) == num31 + num32:
+                    background_p = [805, 200]
+                    background_i = pygame.image.load("Check.gif").convert()
+                    sc.blit(background_i, background_p)
+                    if not 'dora33' in rightans:
+                        rightans.append('dora33')
+                        rightans.append('dora33')  
+                else:
+                    background_p = [805, 200]
+                    background_i = pygame.image.load("none.gif").convert()
+                    sc.blit(background_i, background_p)  
+               
+            if shag == 4:
+                font = pygame.font.Font(None, 120)
+                text = font.render(str(num41) + '  +  ' + str(num42) + '  =', True, (0, 0, 0))
+                sc.blit(text, [200, 203])                
+                if int(numb_ans[0]) == num41 + num42:
+                    background_p = [805, 200]
+                    background_i = pygame.image.load("Check.gif").convert()
+                    sc.blit(background_i, background_p)   
+                  
+                    if not 'dora34' in rightans:
+                        rightans.append('dora34')
+                        rightans.append('dora34')                    
+                else:
+                    background_p = [805, 200]
+                    background_i = pygame.image.load("none.gif").convert()
+                    sc.blit(background_i, background_p)  
+                
+            if shag == 5:
+                font = pygame.font.Font(None, 120)
+                text = font.render(str(num51) + '  +  ' + str(num52) + '  =', True, (0, 0, 0))
+                sc.blit(text, [200, 203])                
+                if int(numb_ans[0]) == num51 + num52:
+                    background_p = [805, 200]
+                    background_i = pygame.image.load("Check.gif").convert()
+                    sc.blit(background_i, background_p) 
+             
+                    if not 'dora35' in rightans:
+                        rightans.append('dora35')
+                        rightans.append('dora35')                     
+                else:
+                    background_p = [805, 200]
+                    background_i = pygame.image.load("none.gif").convert()
+                    sc.blit(background_i, background_p)  
+                    
+        if not 'dora3' in kolgames:
+            kolgames.append('dora3')
+        if step1 == 10:
+            dora3 = False
+            vash_chet = True    
+            
+        
         if start:
             dr1 = image.get_rect(bottomright=(x + 44, y + 40))
             sc.blit(image, dr1)
